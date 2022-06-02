d2ade9c8d0faf1df74049ec1ce77373deb639ca1
Atsighi Bright <brytebee@gmail.com>
2022-06-02 15:17:38 +0100

    :white_check_mark: Update cars_controller
    Add authorization

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CarsController < ApplicationController
   before_action :set_car, only: %i[show update destroy]
+  before_action :authorize_request, except: %i[index show]
 
   # GET /cars
   def index
