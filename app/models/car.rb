ad0be9b1904f437d7e2b7af744c6cefb6e5facd4
Atsighi Bright <brytebee@gmail.com>
2022-06-02 12:23:52 +0100

    :rotating_light: Fixed linters
    Done!

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
 
   validates :make, presence: true
   validates :model, presence: true
-  validates :year, presence: true 
+  validates :year, presence: true
   validates :price, presence: true
   validates :kind, presence: true
   validates :seat, presence: true
