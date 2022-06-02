a48e186573e5170dea01aaf633923adc3881247b
Atsighi Bright <brytebee@gmail.com>
2022-06-02 12:16:35 +0100

    :white_check_mark: Update routes.rb
    Moved cars into the API namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :cars
 namespace :api, defaults: {format: :json} do
   namespace :v1 do
     resources :users, only: %i[index create]
-   
+    resources :cars
   end
 end
 
 post 'api/v1/auth/login' , to: 'api/v1/authentication#login'
-
 
 end
