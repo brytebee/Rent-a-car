4cc590367fcf2c9021474d2018568a0dc17dd276
unknown <mugishasamuel42@gmail.com>
2022-06-02 00:52:31 -0700

    Fix rubocop errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authorize_request, except: :create
 
-before_action :authorize_request, except: :create
-
-
-    def index
-        @users=User.all
-        render json: @users, status: :ok
-    end
-
-  def create
-      @user= User.new(user_params)
-      
-      if @user.save
-        token=JsonWebToken.encode(user_id: @user.id)
-        render json: token, status: :created
-      else
-        render json: {errors:@user.errors.full_messages}, status: :unprocessable_entity
-      end
+  def index
+    @users = User.all
+    render json: @users, status: :ok
   end
 
-  private 
-   def user_params
-    params.permit(:name,:username,:email,:password,:password_confirmation)
-   end
+  def create
+    @user = User.new(user_params)
 
+    if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
+      render json: token, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :username, :email, :password, :password_confirmation)
+  end
 end
