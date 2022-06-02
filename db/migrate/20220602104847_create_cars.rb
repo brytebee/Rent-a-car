014017936abaa6614da0a83a172a4363f0dfe0b3
Atsighi Bright <brytebee@gmail.com>
2022-06-02 11:51:02 +0100

    :white_check_mark: Generate User Model

---
+++ db/migrate/20220602104847_create_cars.rb
@@ -0,0 +1,17 @@
+class CreateCars < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cars do |t|
+      t.string :make
+      t.string :model
+      t.string :color
+      t.integer :year
+      t.string :kind
+      t.integer :seat
+      t.boolean :aircon
+      t.money :price
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
