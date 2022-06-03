fe7f835ec489cdf68fb2952bf79ae1322a5fe101
Atsighi Bright <brytebee@gmail.com>
2022-06-03 10:22:39 +0100

    :recycle: Modify car model
    Removed validations pending unit tests

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,10 @@
 class Car < ApplicationRecord
   belongs_to :user
 
-  validates :make, presence: true
-  validates :model, presence: true
-  validates :year, presence: true
-  validates :price, presence: true
-  validates :kind, presence: true
-  validates :seat, presence: true
-  validates :aircon, presence: true
+  # validates :make, presence: true
+  # validates :model, presence: true
+  # validates :year, presence: true
+  # validates :price, presence: true
+  # validates :engine, presence: true
+  # validates :seat, presence: true
 end
