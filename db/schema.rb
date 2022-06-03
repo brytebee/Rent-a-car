dd86d5aff078fc43ff41c9080a749a50a43962fc
Atsighi Bright <brytebee@gmail.com>
2022-06-03 10:27:25 +0100

    :recycle: Update schema.rb
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_104847) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_091203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,13 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_104847) do
     t.string "image"
     t.string "color"
     t.integer "year"
-    t.string "kind"
+    t.string "engine"
     t.integer "seat"
     t.boolean "aircon"
     t.money "price", scale: 2
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
