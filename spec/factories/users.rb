04654018547a646ad96988999ac485f3a1ee9ab2
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-06 17:52:32 +0530

    Test car controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { 'Robert' }
-    sequence(:username) { |n| "test#{n.to_s.rjust(3, "0")}" }
-    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@gmail.com" }
+    sequence(:username) { |n| "User #{n}" }
+    sequence(:email) { |n| "test-#{n}@gmail.com" }
     password { 'secret123' }
   end
 end
