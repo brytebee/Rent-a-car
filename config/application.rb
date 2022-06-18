656e19eb59e100b9e1a16b3ed2f360d36d3cdc96
Atsighi Bright <brytebee@gmail.com>
2022-06-18 07:01:01 +0100

    :white_check_mark: Rebundle

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,22 @@
 require_relative "boot"
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
+# require "rails"
+# # Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# # require "rails/test_unit/railtie"
+# require "sprockets/railtie"
+
+# Newly added while debugging deploy
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,9 +24,9 @@ Bundler.require(*Rails.groups)
 
 module CarRental
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
