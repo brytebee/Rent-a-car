e2d95ef24e306daaef8cae15f954b2c476b07f7e
Bright Atsighi <brytebee@gmail.com>
2023-03-03 09:40:23 +0100

    Fixed linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  
+  config.fixture_path = '#{::Rails.root}/spec/fixtures'
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
@@ -80,4 +81,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
 end
